54092d5c76bcfd830ea95ccf0fe7542a022c8a18
PMSANTHOSH <145707084+PMSANTHOSH@users.noreply.github.com>
2026-04-26 12:57:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function indexOfIgnoreCase(s1, s2) {
+	 if (subStr === "") return 0;
+
+    str = str.toLowerCase();
+    subStr = subStr.toLowerCase();
+
+    for (let i = 0; i <= str.length - subStr.length; i++) {
+        let j = 0;
+
+        while (j < subStr.length && str[i + j] === subStr[j]) {
+            j++;
+        }
+
+        if (j === subStr.length) return i;
+    }
+
+    return -1;
   // write your code here
 }
 
